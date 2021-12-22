2c1e76efb89b7446068fe8797d4424f312a0c803
Akkie100 <stephan.akkerman@gmail.com>
2021-12-22 21:34:27 +0100

    timeline setup

--- src/cogs/timeline.py
+++ src/cogs/timeline.py
@@ -1,5 +1,7 @@
 ##> Imports
 import asyncio
+import json
+import sys
 
 # > 3rd Party Dependencies
 import tweepy
@@ -12,6 +14,17 @@ from discord.ext import commands
 # Local dependencies
 from config import config
 
+# Set variables
+consumer_key = config['TWITTER']['CONSUMER_KEY']
+consumer_secret = config['TWITTER']['CONSUMER_SECRET']
+access_token = config['TWITTER']['ACCESS_TOKEN_KEY']
+access_token_secret = config['TWITTER']['ACCESS_TOKEN_SECRET']
+
+# Init API
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
 class Timeline(commands.Cog):
     def __init__(self, bot):
         self.bot = bot    
@@ -20,18 +33,68 @@ class Timeline(commands.Cog):
         asyncio.create_task(self.start())
         
     async def start(self):
-        printer = Streamer(config['TWITTER']['CONSUMER_KEY'],
-                           config['TWITTER']['CONSUMER_SECRET'],
-                           config['TWITTER']['ACCESS_TOKEN_KEY'],
-                           config['TWITTER']['ACCESS_TOKEN_SECRET'])
-                        
-        await printer.filter(track=['Twitter'])
+        printer = Streamer(consumer_key, 
+                           consumer_secret,
+                           access_token,
+                           access_token_secret,
+                           self.bot)
+                                                
+        await printer.filter(follow = api.get_friend_ids())
 
 def setup(bot):
     bot.add_cog(Timeline(bot))
 
 class Streamer(AsyncStream):
-    
-    async def on_status(self, status):
-        print(status.id)
+    def __init__(self, consumer_key, consumer_secret, access_token, access_token_secret, bot):
         
+        # Init the parent class
+        AsyncStream.__init__(self, consumer_key, consumer_secret, access_token, access_token_secret)
+        
+        # Set the bot for messages
+        self.bot = bot
+        
+        # Set the channel
+        self.channel = discord.utils.get(
+                        self.bot.get_all_channels(),
+                        guild__name=config["DEBUG"]["GUILD_NAME"]
+                        if len(sys.argv) > 1 and sys.argv[1] == "-test"
+                        else config["DISCORD"]["GUILD_NAME"],
+                        name=config["TIMELINE"]["CHANNEL"],
+                    )
+    
+    async def on_data(self, raw_data):
+        as_json = json.loads(raw_data)
+        
+        user_id = as_json['user']['id']
+        
+        if user_id in api.get_friend_ids():
+        
+            try:
+                text = as_json["extended_tweet"]["full_text"]
+            except Exception:
+                text = as_json["text"]
+                
+            user = as_json["user"]["screen_name"]
+            
+            # Get other info
+            profile_pic = as_json["user"]["profile_image_url"]
+            
+            # Could also use ['id_sr'] instead
+            url = f"https://twitter.com/{user}/status/{as_json['id']}"
+                
+            await self.post_tweet(text, user, profile_pic, url)
+        
+    async def post_tweet(self, text, user, profile_pic, url):
+                
+        # Use 'media' 'url' as url
+        # Use 'profile_image_url'' for thumbnail
+        e = discord.Embed(
+            url=url,
+            description=text,
+            color=0x00FFFF,
+        )
+        
+        e.set_thumbnail(url=profile_pic)
+        e.set_author(name=user, url=url)
+        
+        await self.channel.send(embed=e)
