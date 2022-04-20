b701ffc7d2752fa895ba24b3593af7eb963829eb
Akkie100 <stephan.akkerman@gmail.com>
2022-04-20 10:58:40 +0200

    close #129

--- src/cogs/commands/stock.py
+++ src/cogs/commands/stock.py
@@ -2,6 +2,7 @@
 # > 3rd Party Dependencies
 import pandas as pd
 from discord.ext import commands
+import yfinance as yf
 
 # Local dependencies
 from util.db import get_db, update_db
@@ -25,6 +26,30 @@ class Stock(commands.Cog):
             if input[0] == "add":
                 if len(input) == 3:
                     _, ticker, amount = input
+                    
+                    # Check if this ticker exists
+                    stock_info = yf.Ticker(ticker)
+                    
+                    # If it does not exist let the user know
+                    if stock_info.info['regularMarketPrice'] == None:
+                        confirm_msg = await ctx.send((f"Are you sure {ticker} is correct? We could not find it on Yahoo Finance.\n"
+                                                       "Click on \N{WHITE HEAVY CHECK MARK} to continue and on \N{CROSS MARK} to cancel.")) 
+                        await confirm_msg.add_reaction("\N{WHITE HEAVY CHECK MARK}")
+                        await confirm_msg.add_reaction("\N{CROSS MARK}")
+                        
+                        # Handle preview accept/deny using reactions
+                        reaction = await self.bot.wait_for(
+                            "reaction_add",
+                            check=lambda r, u: (
+                                str(r.emoji) == "\N{WHITE HEAVY CHECK MARK}"
+                                or str(r.emoji) == "\N{CROSS MARK}"
+                            )
+                            and u == ctx.author,
+                        )
+                        
+                        if reaction[0].emoji == "\N{CROSS MARK}":
+                            await ctx.send(f"Did not add {ticker} to the database.")
+                            return
 
                     # Add ticker to database
                     new_data = pd.DataFrame(
