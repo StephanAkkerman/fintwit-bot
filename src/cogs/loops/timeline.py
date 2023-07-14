e9f9557903b0eecd7147f32187f318f435f16320
Akkie100 <stephan.akkerman@gmail.com>
2023-07-14 17:19:25 +0200

    Improved code readability

--- src/cogs/loops/timeline.py
+++ src/cogs/loops/timeline.py
@@ -1,18 +1,12 @@
-##> Imports
-# > Standard libraries
 from __future__ import annotations
-from typing import List
+from typing import List, Optional
 import traceback
 
-# > 3rd Party Dependencies
 import aiohttp
-
-# > Discord dependencies
 import discord
 from discord.ext import commands
 from discord.ext.tasks import loop
 
-# Local dependencies
 from util.vars import config
 from util.disc_util import get_channel, get_tagged_users, get_webhook
 from util.tweet_embed import make_tweet_embed
@@ -23,88 +17,56 @@ from util.get_tweet import get_tweet
 class Timeline(commands.Cog):
     """
     The main Class of this project. This class is responsible for streaming tweets from the Twitter API.
-
-    Methods
-    -------
-    all_txt_channels()
-        Gets all the text channels as Discord object and the names of the channels.
-    get_following_ids()
-        Gets the Twitters IDs of the accounts that the bot is following.
-    on_data(raw_data : str)
-        This method is called whenever data is received from the stream.
-    post_tweet(text, user, profile_pic, url, images, tickers, hashtags, retweeted_user)
-        Formats the tweet and passes it to upload_tweet().
-    upload_tweet(e, category, images, user, retweeted_user)
-        Uploads the tweet to the correct channel.
     """
 
     def __init__(self, bot: commands.Bot) -> None:
+        """Initializes the Timeline class.
+
+        Parameters
+        ----------
+        bot : commands.Bot
+            The bot object from discord.py
+        """
         self.bot = bot
 
         charts_channel = config["LOOPS"]["TIMELINE"]["CHARTS_CHANNEL"]
         text_channel = config["LOOPS"]["TIMELINE"]["TEXT_CHANNEL"]
 
         # Set the channels
-        if config["LOOPS"]["TIMELINE"]["STOCKS"]["ENABLED"]:
-            self.stocks_charts_channel = get_channel(
-                self.bot, charts_channel, config["CATEGORIES"]["STOCKS"]
-            )
-            self.stocks_text_channel = get_channel(
-                self.bot, text_channel, config["CATEGORIES"]["STOCKS"]
-            )
-
-        if config["LOOPS"]["TIMELINE"]["CRYPTO"]["ENABLED"]:
-            self.crypto_charts_channel = get_channel(
-                self.bot, charts_channel, config["CATEGORIES"]["CRYPTO"]
-            )
-            self.crypto_text_channel = get_channel(
-                self.bot, text_channel, config["CATEGORIES"]["CRYPTO"]
-            )
-
-        if config["LOOPS"]["TIMELINE"]["FOREX"]["ENABLED"]:
-            self.forex_charts_channel = get_channel(
-                self.bot, charts_channel, config["CATEGORIES"]["FOREX"]
-            )
-            self.forex_text_channel = get_channel(
-                self.bot, text_channel, config["CATEGORIES"]["FOREX"]
-            )
-
-        if config["LOOPS"]["TIMELINE"]["IMAGES"]["ENABLED"]:
-            self.images_channel = get_channel(
-                self.bot, config["LOOPS"]["TIMELINE"]["IMAGES"]["CHANNEL"]
-            )
-        if config["LOOPS"]["TIMELINE"]["OTHER"]["ENABLED"]:
-            self.other_channel = get_channel(
-                self.bot, config["LOOPS"]["TIMELINE"]["OTHER"]["CHANNEL"]
-            )
-        if config["LOOPS"]["TIMELINE"]["NEWS"]["ENABLED"]:
-            self.news_channel = get_channel(
-                self.bot,
-                config["LOOPS"]["TIMELINE"]["NEWS"]["CHANNEL"],
-                config["CATEGORIES"]["TWITTER"],
-            )
-
-            if config["LOOPS"]["TIMELINE"]["NEWS"]["CRYPTO"]["ENABLED"]:
-                self.crypto_news_channel = get_channel(
-                    self.bot,
-                    config["LOOPS"]["TIMELINE"]["NEWS"]["CHANNEL"],
-                    config["CATEGORIES"]["CRYPTO"],
-                )
+        self.set_channels("STOCKS", charts_channel, text_channel)
+        self.set_channels("CRYPTO", charts_channel, text_channel)
+        self.set_channels("FOREX", charts_channel, text_channel)
+        self.set_channels("IMAGES", charts_channel, text_channel)
+        self.set_channels("OTHER", charts_channel, text_channel)
+        self.set_channels("NEWS", charts_channel, text_channel)
 
         # Get all text channels
         self.all_txt_channels.start()
-
         self.get_latest_tweet.start()
+
+    def set_channels(self, name: str, charts_channel: str, text_channel: str) -> None:
+        """Set channels for each category.
+
+        Parameters
+        ----------
+        name : str
+            The name of the category.
+        charts_channel : str
+            The name of the charts channel.
+        text_channel : str
+            The name of the text channel.
+        """
+        if config["LOOPS"]["TIMELINE"][name]["ENABLED"]:
+            self.__dict__[f"{name.lower()}_charts_channel"] = get_channel(
+                self.bot, charts_channel, config["CATEGORIES"][name]
+            )
+            self.__dict__[f"{name.lower()}_text_channel"] = get_channel(
+                self.bot, text_channel, config["CATEGORIES"][name]
+            )
 
     @loop(hours=1)
     async def all_txt_channels(self) -> None:
-        """
-        Gets all the text channels as Discord object and the names of the channels.
-
-        Returns
-        -------
-        None
-        """
+        """Gets all the text channels as Discord object and the names of the channels."""
         self.following_ids = []
 
         text_channel_list = []
@@ -122,8 +84,8 @@ class Timeline(commands.Cog):
         self.text_channel_names = text_channel_names
 
     @loop(minutes=5)
-    async def get_latest_tweet(self):
-        print("Getting latest tweets")
+    async def get_latest_tweet(self) -> None:
+        """Fetches the latest tweets."""
         tweets = await get_tweet()
         last_tweet = len(tweets) - 1
         print(f"Got {last_tweet} tweets")
@@ -139,22 +101,18 @@ class Timeline(commands.Cog):
             await self.on_data(tweet, update_tweet_id=True)
 
     async def on_data(self, tweet: dict, update_tweet_id: bool = False) -> None:
-        """
+        """This method is called whenever data is received from the stream.
+
         Parameters
         ----------
-        raw_data : str
-            The raw data received from the stream in json text format.
-
-        Returns
-        -------
-        None
+        tweet : dict
+            The raw tweet data.
+        update_tweet_id : bool, optional
+            Whether or not to update the tweet ID, by default False
         """
-
         formatted_tweet = parse_tweet(tweet, update_tweet_id=update_tweet_id)
 
-        if formatted_tweet == None:
-            return
-        else:
+        if formatted_tweet is not None:
             (
                 text,
                 user_name,
@@ -186,34 +144,26 @@ class Timeline(commands.Cog):
     async def upload_tweet(
         self,
         e: discord.Embed,
-        category: str,
+        category: Optional[str],
         media: List[str],
         user_screen_name: str,
         tickers: List[str],
     ) -> None:
-        """
-        Uploads tweet in the dedicated Discord channel.
+        """Uploads tweet in the dedicated Discord channel.
 
         Parameters
         ----------
-            e : discord.Embed
-                The Tweet as a Discord embed object.
-            category : str
-                The category of the tweet, used to decide which Discord channel it should be uploaded to.
-            images : list
-                The images contained in this tweet.
-            user : str
-                The user that posted this tweet.
-            retweeted_user : str
-                The user that was retweeted by this tweet.
-            tickers : List[str]
-                The list of tickers contained in this tweet.
-
-        Returns
-        -------
-        None
+        e : discord.Embed
+            The Tweet as a Discord embed object.
+        category : str, optional
+            The category of the tweet, used to decide which Discord channel it should be uploaded to.
+        media : list
+            The images contained in this tweet.
+        user_screen_name : str
+            The user that posted this tweet.
+        tickers : list
+            The list of tickers contained in this tweet.
         """
-
         user_channel = None
 
         # Default channel
@@ -228,58 +178,67 @@ class Timeline(commands.Cog):
         # News posters (Do not post news in other channels)
         if user_screen_name in config["LOOPS"]["TIMELINE"]["NEWS"]["FOLLOWING"]:
             channel = self.news_channel
-            await self.post_tweet(channel, e, media, tickers, user_channel, category)
-            return
-
-        if (
+        elif (
             user_screen_name
             in config["LOOPS"]["TIMELINE"]["NEWS"]["CRYPTO"]["FOLLOWING"]
         ):
             channel = self.crypto_news_channel
-            await self.post_tweet(channel, e, media, tickers, user_channel, category)
-            return
-
-        # Tweets without financial information
-        if category == None and not media:
-            channel = self.other_channel
-        if category == None and media:
-            channel = self.images_channel
-
-        # If we do not know what category it is, assume it is crypto
-        if (category == "crypto" or category == "🤷‍♂️") and not media:
-            channel = self.crypto_text_channel
-        if (category == "crypto" or category == "🤷‍♂️") and media:
-            channel = self.crypto_charts_channel
-
-        # Stocks tweet channels
-        if category == "stocks" and not media:
-            channel = self.stocks_text_channel
-        if category == "stocks" and media:
-            channel = self.stocks_charts_channel
-
-        # Forex tweet channels
-        if category == "forex" and not media:
-            channel = self.forex_text_channel
-        if category == "forex" and media:
-            channel = self.forex_charts_channel
+        else:
+            channel = self.get_channel_based_on_category(category, media)
 
         await self.post_tweet(channel, e, media, tickers, user_channel, category)
 
-    async def make_and_send_webhook(self, channel, tickers, image_e):
-        webhook = await get_webhook(channel)
+    def get_channel_based_on_category(
+        self, category: Optional[str], media: List[str]
+    ) -> discord.abc.GuildChannel:
+        """Get the Discord channel based on the category of the tweet.
 
-        # Wait so we can use this message as reference
-        msg = await webhook.send(
-            content=get_tagged_users(tickers),
-            embeds=image_e,
-            username="FinTwit",
-            wait=True,
-            avatar_url=self.bot.user.avatar.url,
-        )
+        Parameters
+        ----------
+        category : str, optional
+            The category of the tweet.
+        media : list
+            The images contained in this tweet.
 
-        return msg
+        Returns
+        -------
+        discord.abc.GuildChannel
+            The Discord channel.
+        """
+        if category is None:
+            channel = self.images_channel if media else self.other_channel
+        else:
+            channel_type = "charts" if media else "text"
+            channel = self.__dict__[f"{category}_{channel_type}_channel"]
 
-    async def post_tweet(self, channel, e, media, tickers, user_channel, category):
+        return channel
+
+    async def post_tweet(
+        self,
+        channel: discord.abc.GuildChannel,
+        e: discord.Embed,
+        media: List[str],
+        tickers: List[str],
+        user_channel: Optional[discord.abc.GuildChannel],
+        category: Optional[str],
+    ) -> None:
+        """Formats the tweet and passes it to upload_tweet().
+
+        Parameters
+        ----------
+        channel : discord.abc.GuildChannel
+            The Discord channel where the tweet should be posted.
+        e : discord.Embed
+            The Tweet as a Discord embed object.
+        media : list
+            The images contained in this tweet.
+        tickers : list
+            The list of tickers contained in this tweet.
+        user_channel : discord.abc.GuildChannel, optional
+            The user-specific Discord channel.
+        category : str, optional
+            The category of the tweet.
+        """
         msgs = []
 
         try:
@@ -329,6 +288,41 @@ class Timeline(commands.Cog):
         except Exception as error:
             print("Error posting tweet on timeline", error)
             print(traceback.format_exc())
+
+    async def make_and_send_webhook(
+        self,
+        channel: discord.abc.GuildChannel,
+        tickers: List[str],
+        image_e: List[discord.Embed],
+    ) -> discord.Message:
+        """Creates and sends a webhook.
+
+        Parameters
+        ----------
+        channel : discord.abc.GuildChannel
+            The Discord channel.
+        tickers : list
+            The list of tickers contained in this tweet.
+        image_e : list
+            The images contained in this tweet.
+
+        Returns
+        -------
+        discord.Message
+            The Discord message.
+        """
+        webhook = await get_webhook(channel)
+
+        # Wait so we can use this message as reference
+        msg = await webhook.send(
+            content=get_tagged_users(tickers),
+            embeds=image_e,
+            username="FinTwit",
+            wait=True,
+            avatar_url=self.bot.user.avatar.url,
+        )
+
+        return msg
 
 
 def setup(bot: commands.Bot) -> None:
--- src/util/parse_tweet.py
+++ src/util/parse_tweet.py
@@ -1,110 +1,128 @@
 import re
+from typing import List, Tuple, Optional
 
+# > Local imports
 import util.vars
 
 
-def remove_twitter_url_at_end(text):
-    pattern = r"(https?://t\.co/\S+)$"  # Regular expression pattern to match t.co URLs at the end
+def remove_twitter_url_at_end(text: str) -> str:
+    """
+    Removes a t.co URL at the end of a text string.
+
+    Parameters
+    ----------
+    text : str
+        The text from which to remove the URL.
+
+    Returns
+    -------
+    str
+        The text with the URL removed.
+    """
+    pattern = r"(https?://t\.co/\S+)$"
     return re.sub(pattern, "", text)
 
 
-def parse_tweet(tweet: dict, update_tweet_id: bool = False):
+def get_legacy_info(tweet: dict, key: str) -> Optional[str]:
+    """
+    Retrieves legacy information from a tweet.
+
+    Parameters
+    ----------
+    tweet : dict
+        The tweet from which to retrieve information.
+    key : str
+        The key of the information to retrieve.
+
+    Returns
+    -------
+    Optional[str]
+        The retrieved information, or None if the key does not exist.
+    """
+    return tweet["core"]["user_results"]["result"]["legacy"].get(key)
+
+
+def get_entities(tweet: dict, key: str) -> List[str]:
+    """
+    Retrieves entities from a tweet.
+
+    Parameters
+    ----------
+    tweet : dict
+        The tweet from which to retrieve entities.
+    key : str
+        The key of the entities to retrieve.
+
+    Returns
+    -------
+    List[str]
+        The retrieved entities, or an empty list if the key does not exist.
+    """
+    entities = tweet["legacy"]["entities"].get(key)
+    return [entity["text"] for entity in entities] if entities else []
+
+
+def parse_tweet(
+    tweet: dict, update_tweet_id: bool = False
+) -> Tuple[str, str, str, str, str, List[str], List[str], List[str], Optional[str]]:
+    """
+    Parses a tweet and returns a tuple with all important information.
+
+    Parameters
+    ----------
+    tweet : dict
+        The tweet to parse.
+    update_tweet_id : bool, optional
+        If this Tweet should update the database, by default False.
+
+    Returns
+    -------
+    Tuple[str, str, str, str, str, List[str], List[str], List[str], Optional[str]]
+        The parsed tweet.
+    """
     reply = None
 
-    if "items" in tweet.keys():
-        reply = tweet["items"][1]["item"]["itemContent"]["tweet_results"]
-        tweet = tweet["items"][0]["item"]["itemContent"]["tweet_results"]
+    if "items" in tweet:
+        reply = tweet["items"][1]["item"]["itemContent"].get("tweet_results")
+        tweet = tweet["items"][0]["item"]["itemContent"].get("tweet_results")
+    elif "itemContent" in tweet:
+        tweet = tweet["itemContent"].get("tweet_results")
 
-    elif "itemContent" in tweet.keys():
-        if "tweet_results" in tweet["itemContent"]:
-            tweet = tweet["itemContent"]["tweet_results"]
-        else:
-            print(tweet)
+    tweet = tweet.get("result")
 
-    tweet = tweet["result"]
+    tweet_id = int(tweet.get("legacy", {}).get("id_str", tweet["tweet"]["rest_id"]))
 
-    # Ignore Tweets that are older than the latest tweet
-    if "legacy" not in tweet:
-        tweet_id = int(tweet["tweet"]["rest_id"])
-    else:
-        tweet_id = int(tweet["legacy"]["id_str"])
-
-    if "core" not in tweet:
-        tweet = tweet["tweet"]
-
-    # So we can use this function recursively
     if update_tweet_id:
-        # Skip this tweet
         if tweet_id <= util.vars.latest_tweet_id:
             return
         util.vars.latest_tweet_id = tweet_id
 
-    # Default is no retweet
-    retweeted_user = None
+    tweet = tweet.get("core", tweet)
 
-    # Get user info
-    user_name = tweet["core"]["user_results"]["result"]["legacy"][
-        "name"
-    ]  # The name of the account (not @username)
-    user_screen_name = tweet["core"]["user_results"]["result"]["legacy"][
-        "screen_name"
-    ]  # The @username
-    user_img = tweet["core"]["user_results"]["result"]["legacy"][
-        "profile_image_url_https"
-    ]
+    user_name = get_legacy_info(tweet, "name")
+    user_screen_name = get_legacy_info(tweet, "screen_name")
+    user_img = get_legacy_info(tweet, "profile_image_url_https")
 
-    # Media
     media = []
-    if "extended_entities" in tweet["legacy"].keys():
-        if "media" in tweet["legacy"]["extended_entities"].keys():
-            media = [
-                image["media_url_https"]
-                for image in tweet["legacy"]["extended_entities"]["media"]
-            ]
+    if "extended_entities" in tweet["legacy"]:
+        media = [
+            image["media_url_https"]
+            for image in tweet["legacy"]["extended_entities"].get("media", [])
+        ]
 
-    # Text
     text = tweet["legacy"]["full_text"]
+    text = remove_twitter_url_at_end(text)
 
-    # Tweet url
     tweet_url = f"https://twitter.com/user/status/{tweet_id}"
 
-    # Tickers
-    tickers = tweet["legacy"]["entities"]["symbols"]
-    if tickers:
-        tickers = [ticker["text"] for ticker in tickers]
+    tickers = get_entities(tweet, "symbols")
+    hashtags = get_entities(tweet, "hashtags")
 
-    # Hashtags
-    hashtags = tweet["legacy"]["entities"]["hashtags"]
-    if hashtags:
-        hashtags = [hashtag["text"] for hashtag in hashtags]
+    quoted_status_result = tweet.get("quoted_status_result")
+    retweeted_status_result = tweet["legacy"].get("retweeted_status_result")
 
-    # Quote tweet
-    if "quoted_status_result" in tweet.keys():
-        (
-            q_text,
-            q_user_name,
-            q_user_screen_name,
-            _,
-            _,
-            q_media,
-            q_tickers,
-            q_hashtags,
-            _,
-        ) = parse_tweet(tweet["quoted_status_result"])
-
-        # Format the text to add the quoted tweet text
-        q_text = "\n".join(map(lambda line: "> " + line, q_text.split("\n")))
-        text = f"{text}\n\n> [@{q_user_screen_name}](https://twitter.com/{q_user_screen_name}):\n{q_text}"
-
-        # Add media, tickers, and hashtags together
-        media += q_media
-        tickers += q_tickers
-        hashtags += q_hashtags
-
-        retweeted_user = q_user_name
-
-    if "retweeted_status_result" in tweet["legacy"].keys():
-        # Get retweeted_info
+    if quoted_status_result or retweeted_status_result or reply:
+        result = quoted_status_result or retweeted_status_result or reply
         (
             r_text,
             r_user_name,
@@ -115,59 +133,23 @@ def parse_tweet(tweet: dict, update_tweet_id: bool = False):
             r_tickers,
             r_hashtags,
             _,
-        ) = parse_tweet(tweet["legacy"]["retweeted_status_result"])
-
-        # Overwrite text
-        text = r_text
-        retweeted_user = r_user_name
-
-    if reply:
-        # Get reply info
-        (
-            r_text,
-            r_user_name,
-            r_user_screen_name,
-            _,
-            _,
-            r_media,
-            r_tickers,
-            r_hashtags,
-            _,
-        ) = parse_tweet(reply)
+        ) = parse_tweet(result)
 
         text = "\n".join(map(lambda line: "> " + line, text.split("\n")))
         text = f"> [@{r_user_screen_name}](https://twitter.com/{r_user_screen_name}):\n{text}\n\n{r_text}"
 
-        # Add media, tickers, and hashtags together
         media += r_media
         tickers += r_tickers
         hashtags += r_hashtags
 
-        # Disable retweeted_user if reply
-        retweeted_user = None
+    text = text.replace("&amp;", "&").replace("&gt;", ">").replace("&lt;", "<")
 
-    # Remove t.co url from text
-    text = remove_twitter_url_at_end(text)
-
-    # Replace &amp; etc.
-    text = text.replace("&amp;", "&")
-    text = text.replace("&gt;", ">")
-    text = text.replace("&lt;", "<")
-
-    # Convert media, tickers, hasthtags to sets to remove duplicates
     media = list(set(media))
     tickers = list(set(tickers))
     hashtags = list(set(hashtags))
 
-    # tickers and hashtags all uppercase
     tickers = [ticker.upper() for ticker in tickers]
-    hashtags = [hashtag.upper() for hashtag in hashtags]
-
-    # Remove #crypto
-    hashtags = [hashtag for hashtag in hashtags if hashtag != "CRYPTO"]
-
-    # Maybe create the Discord title here as well
-    # title = ...
+    hashtags = [hashtag.upper() for hashtag in hashtags if hashtag != "CRYPTO"]
 
     return (
         text,
@@ -178,5 +160,5 @@ def parse_tweet(tweet: dict, update_tweet_id: bool = False):
         media,
         tickers,
         hashtags,
-        retweeted_user,
+        r_user_name if reply else None,
     )
